45502331fd22f9729cab29a9b25d35b4140a448c
Raihan Bin Solaiman <arraihan4897@gmail.com>
2023-02-11 01:39:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const inputEl = document.querySelector('.btn');
+
+
+console.log(inputEl.checked);
+
+// inputEl.checked= true;
+
+
+// function UpdateBody() {
+    
+// }
+
+
+
+
+AOS.init({
+    duration: 3000,
+    once: true,
+  });
